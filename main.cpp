99516b062ca153f2d9e04f8694cf56c8ba3cd550
vinnytmv <valopez@mail.csuchico.edu>
2018-09-17 17:56:14 -0700

    Enables userto determine if they chose correct number

--- main.cpp
+++ main.cpp
@@ -6,19 +6,19 @@ using namespace std;
 int main()
 {
   int guess;
-
   cout<<"Guess a number between 1-10: ";
   cin>>guess;
-
-
-  //when they guess correctly (7), display:
-  cout<<"YES! Lucky number 7!\n";
-
-  //when they guess just off by one (either 6 or 8), display:
-  cout<<"Oh, so close!\n";
-
-  //when they guess any other number, display:
-  cout<<"Waaay off, sorry.\n";
-
+    if (guess == 7)
+      {
+cout<<"YES! Lucky number 7!\n";
+      }
+    else if (guess == 6 ||  guess == 8)
+      {
+cout<<"Oh, so close!\n";
+      }
+    else 
+      {
+cout<<"Waaay off, sorry.\n";
+      }
   return 0;
 }
